b86c5acd8f21e0fd8db66551197af71c547d2e5f
Samet Yasıt <120249031+sametyasit@users.noreply.github.com>
2024-11-03 11:51:03 +0300

    Add files via upload

---
+++ TabBarKullanimi/TabBarKullanimi/AyarlarVC.swift
@@ -0,0 +1,18 @@
+//
+//  AyarlarVC.swift
+//  TabBarKullanimi
+//
+//  Created by Kasım on 8.08.2023.
+//
+
+import UIKit
+
+class AyarlarVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
---
+++ TabBarKullanimi/TabBarKullanimi/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  TabBarKullanimi
+//
+//  Created by Kasım on 8.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let tabItems = tabBarController?.tabBar.items {
+            let anasayfaItem = tabItems[0]
+            let ayarlarItem = tabItems[1]
+            anasayfaItem.badgeValue = "3"
+            ayarlarItem.badgeValue = "Yeni"
+        }
+        
+        let appearance = UITabBarAppearance()
+        appearance.backgroundColor = UIColor.systemIndigo
+        
+        renkDegistir(itemAppearance: appearance.stackedLayoutAppearance)
+        renkDegistir(itemAppearance: appearance.inlineLayoutAppearance)
+        renkDegistir(itemAppearance: appearance.compactInlineLayoutAppearance)
+        
+        tabBarController?.tabBar.standardAppearance = appearance
+        tabBarController?.tabBar.scrollEdgeAppearance = appearance
+        
+    }
+
+    func renkDegistir(itemAppearance:UITabBarItemAppearance){
+        //Seçili durum
+        itemAppearance.selected.iconColor = UIColor.systemOrange
+        itemAppearance.selected.titleTextAttributes = [.foregroundColor: UIColor.systemOrange]
+        itemAppearance.selected.badgeBackgroundColor = UIColor.systemMint
+        //Seçili olmayan durum
+        itemAppearance.normal.iconColor = UIColor.white
+        itemAppearance.normal.titleTextAttributes = [.foregroundColor: UIColor.white]
+        itemAppearance.normal.badgeBackgroundColor = UIColor.lightGray
+    }
+
+}
+
